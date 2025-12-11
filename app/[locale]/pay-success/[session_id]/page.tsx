09f7f4787996b2bebc5874745edd518f483aa8c4
Yiiii0 <yiqiaozhou@outlook.com>
2025-12-10 21:25:40 -0500

    fix: update Stripe API version to 2025-02-24.acacia

--- app/[locale]/pay-success/[session_id]/page.tsx
+++ app/[locale]/pay-success/[session_id]/page.tsx
@@ -7,7 +7,7 @@ import { updateCreditForOrder } from "@/services/credit";
 import { findOrderBySessionId } from "@/models/order";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
 });
 
 export default async function PaySuccessPage({ 
